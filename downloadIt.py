25ae534a1d39e8156d6e9fad40081e6292ae2155
Mahesh S R <mahepj94@gmail.com>
2015-07-12 16:57:17 +0530

    complete ok/cancel button

--- downloadIt.py
+++ downloadIt.py
@@ -6,8 +6,10 @@ Author : Mahesh S.R
 Email : mahepj94@gmail.com
 """
 
+from multiprocessing import Process, Queue
 from bs4 import BeautifulSoup
 from PyQt4 import QtGui
+import urllib
 import requests
 import signal
 import sys
@@ -26,6 +28,8 @@ class Example(QtGui.QWidget):
         url = QtGui.QLabel('Website')
         album = QtGui.QLabel('Album')
         
+        self.result = QtGui.QLabel(self)
+        
         self.okButton = QtGui.QPushButton("OK")
         self.okButton.clicked.connect(self.start_download)
         
@@ -41,7 +45,9 @@ class Example(QtGui.QWidget):
         grid.addWidget(album, 2, 0)
         grid.addWidget(self.albumEdit, 2, 1)
 
-        grid.addWidget(self.okButton, 3, 1)
+	grid.addWidget(self.result, 3, 1)
+	
+        grid.addWidget(self.okButton, 4, 1)
         
         self.setLayout(grid) 
         
@@ -50,25 +56,43 @@ class Example(QtGui.QWidget):
         self.show()
     	
     def start_download(self):
-    	self.child_pid = os.fork()
-	if self.child_pid != 0:
-		self.okButton.setText("Cancel Download")
-		self.okButton.clicked.connect(self.cancel_download)
-	else:
-		self.download()
+    
+    	
+	self.p = Process(target=self.download, args=())
+	self.p.start()
+	
+	self.okButton.setText("Cancel Download")
+	self.okButton.clicked.disconnect()
+	self.okButton.clicked.connect(self.cancel_download)
+	
+	self.p.join()
+		
+	self.result.setText("Downloading complete")
+		
+	self.okButton.setText("OK")
+	self.okButton.clicked.disconnect()
+	self.okButton.clicked.connect(self.start_download)
+	
 		
     def cancel_download(self):
-   	os.kill(self.child_pid, signal.SIGTERM)
+    	os.kill(self.p.pid, signal.SIGKILL)
+    	
+    	self.result.setText("Cancelled")
+    	
 	self.okButton.setText("OK")
+	self.okButton.clicked.disconnect()
 	self.okButton.clicked.connect(self.start_download)
         
     def download(self):
-    
+    	'''    
     	url = str(self.urlEdit.displayText())
     	album = str(self.albumEdit.displayText())
-        
-        base_url = "http://www.gamethemesongs.com/"
+	'''
 
+	url = "http://www.gamethemesongs.com/a-theme-songs.html"
+	album = "Assassins Creed Unity"
+	        
+        base_url = "http://www.gamethemesongs.com/"
 
 	if("http://" not in url):
 		r  = requests.get("http://" + url)
@@ -80,8 +104,9 @@ class Example(QtGui.QWidget):
 
 	album_folder = re.escape(album)
 
-	os.system("mkdir " + album_folder)
-
+	if not(os.path.isdir(album)):
+		os.system("mkdir " + album_folder)
+	
 	for sound in soup.find_all('a'):
 		if(album in sound.get_text()):		
 			r  = requests.get(sound.get("href"))
@@ -92,8 +117,13 @@ class Example(QtGui.QWidget):
 			download_link = new_soup.find_all("a", "download")[0].get("href")
 		
 			file_name = download_link.replace("download.php?f=", "")
-		
+			
+			urllib.urlretrieve(base_url + download_link, filename = album + "/" + file_name)
+			'''
 			os.system("wget -O " + album_folder + "/" + file_name + " " + base_url + download_link)
+			'''
+			break
+	
 		
 
 
